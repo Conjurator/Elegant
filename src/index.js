4788c3193c282f97f797f95182340b1603426afe
haohaiwang <haohaiwang@xiaomi.com>
2017-11-01 17:11:51 +0800

    add function export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import format from './format.js'
+import cartNumber from './cartNumber'
+import compose from './compose'
+import curry from './curry'
+import memorize from './memorize'
+import random from './random'
 
 export default {
-    format
+    format,
+    cartNumber,
+    compose,
+    curry,
+    memorize,
+    random
 }
