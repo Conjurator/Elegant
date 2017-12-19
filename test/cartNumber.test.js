13d22745b25d7600df9402aebf4598c1400b4e09
sjffly <1982764150@qq.com>
2017-12-19 11:35:55 +0800

    fix the bug of isOrientation and optimize the cartNumber and remove transferBool

--- test/cartNumber.test.js
+++ test/cartNumber.test.js
@@ -4,7 +4,7 @@ import cartNumber from '../src/cartNumber';
 describe('cartNumber function test', () => {
     it('should return undefined', () => {
         let result = cartNumber('abc');
-        expect(result).to.equal(undefined);
+        expect(result).to.equal(0);
     })
 
     it('should return 100', () => {
@@ -14,7 +14,7 @@ describe('cartNumber function test', () => {
 
     it('should return 12.0万', () => {
         let result = cartNumber(120000);
-        expect(result).to.equal('12.0万');
+        expect(result).to.equal('12万');
     })
 
     it('should return 1.2亿', () => {
@@ -24,6 +24,11 @@ describe('cartNumber function test', () => {
 
     it('should return 1.200亿', () => {
         let result = cartNumber(120000000,3);
-        expect(result).to.equal('1.200亿');
+        expect(result).to.equal('1.2亿');
+    })
+
+    it('should return 1.211亿', () => {
+        let result = cartNumber(121100000,3);
+        expect(result).to.equal('1.211亿');
     })
 })
