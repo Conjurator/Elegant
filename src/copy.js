d7adac9ab8f000bd8002571202a729dac096db43
qilianshan@git.n.xiaomi.com <qilianshan@git.n.xiaomi.com>
2018-01-19 11:46:10 +0800

    add context
    Signed-off-by: qilianshan@git.n.xiaomi.com <qilianshan@git.n.xiaomi.com>

--- src/copy.js
+++ src/copy.js
@@ -1,13 +1,24 @@
+/**
+ * copy JSON
+ *
+ * @param {fns} Objcet
+ * @returns {Object} Return a new copy
+ * @example copy(myObject,true)
+ *  => (Object)obj
+ *
+ */
 import judgeType from './.internal/judgeType'
 
 const find = (list, fn) => list.filter(fn)[0]
 
 const copy = (source, deep = false, cache = []) => {
     let sourceType = judgeType(source)
+
     if (!sourceType('array') && !sourceType('object')) {
         return source
     }
 
+    //test if recursive reference exist,marked as "hit"
     let hit = find(cache, val => val.origin === source)
     if (hit) {
         return hit.copy
--- src/debounce.js
+++ src/debounce.js
@@ -1,3 +1,17 @@
+/**
+ * a strict curried function accept arguments one by one,every argument can return
+ × a function which accept next argument
+ *
+ * @param {function} fn the function will be called
+ * @param {number} 
+ * @param {boolean} excute function imediately
+ * @param {object} function running context
+ * @returns {function} Return the curried function, which accept one argument
+ * @example
+ *
+ * fn(arg1, arg2, arg3)
+ * // => curry(fn)(arg1)(arg2)(arg3)
+ */
 const debounce = function (func, wait, immediate, funcContext) {
     var args, context, timestamp, result
 
